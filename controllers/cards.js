9aac094bb855ce6f8343bd0c4ac3ead8b8259c3b
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-13 04:03:07 +0700

    Sprint 13. Errors ready.

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,21 @@
 // Методы работы с карточками
 const Card = require('../models/card');
 
+const errorReturn = (res, err) => {
+  if (err.name === 'CastError') {
+    return res.status(404).send({ message: 'Карточка не найдена' });
+  }
+  else if (err.name === 'ValidationError') {
+    return res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки' });
+  }
+  return res.status(500).send({ message: 'Произошла ошибка' });
+};
+
 const getAllCards = (req, res) => {
   Card.find({})
-    .then(user => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(card => res.send({ data: card }))
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 }
 
 const createCard = (req, res) => {
@@ -12,25 +23,29 @@ const createCard = (req, res) => {
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 }
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 const unlikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,51 @@
 const User = require('../models/user');
 
+const errorReturn = (res, err) => {
+  if (err.name === 'CastError') {
+    return res.status(404).send({ message: 'Пользователь не найден' });
+  }
+  else if (err.name === 'ValidationError') {
+    return res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя' });
+  }
+  return res.status(500).send({ message: 'Произошла ошибка' });
+};
+
 const getAllUsers = (req, res) => {
   User.find({ })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 }
 
 const getOneUser = (req, res) => {
   User.findById(req.params.id)
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name: name }, { about: about })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 const updateAvatar = (req, res) => {
   const { name, avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { name: name }, { avatar: avatar })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    //.catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => errorReturn(res, err));
 };
 
 module.exports = {
