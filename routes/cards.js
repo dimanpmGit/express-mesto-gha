ca9e9507454451c8008ab223271462dc63a0c955
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-13 16:56:18 +0700

    Sprint 13. Errors ready.

--- routes/cards.js
+++ routes/cards.js
@@ -7,5 +7,12 @@ router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', unlikeCard);
+router.use('/*', (req, res) => res.status(404).send({ message: "Страница не найдена" }));
+
+/*router.get('/', wrongUrl);
+router.post('/', wrongUrl);
+router.put('/', wrongUrl);
+router.patch('/', wrongUrl);
+router.delete('/', wrongUrl);*/
 
 module.exports = router;
