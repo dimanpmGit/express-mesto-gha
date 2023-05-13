475c680779795eef20ea78e4520dce46d5350202
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-13 16:35:22 +0700

    Sprint 13. Errors ready.

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,24 +1,23 @@
 // Методы работы с карточками
 const Card = require('../models/card');
 
-class IncorrectCardDataError extends Error {
+class CardFindError extends Error {
   constructor(message) {
     super(message);
-    this.name = "IncorrectCardDataError";
-    this.statusCode = 400;
+    this.name = "CardFindError";
+    this.statusCode = 404;
   }
 }
 
 const errorReturn = (res, err) => {
-  console.log(err);
   if (err.name === 'CastError') {
-    return res.status(400).send({ message: 'Карточка не найдена' });
+    return res.status(400).send({ message: 'Переданы некорректные данные карточки при лайке' });
   }
   else if (err.name === 'ValidationError') {
     return res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки' });
   }
-  else if (err.name === 'IncorrectCardDataError') {
-    return res.status(400).send({ message: err.message });
+  else if (err.name === 'CardFindError') {
+    return res.status(404).send({ message: err.message });
   }
   return res.status(500).send({ message: 'Произошла ошибка' });
 };
@@ -39,24 +38,37 @@ const createCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then(card => res.send({ data: card }))
+    .then(card => {
+      if (card) {
+        return res.send({ data: card })
+      };
+      return Promise.reject(new CardFindError('Карточка не найден'));
+      res.send({ data: card })
+    })
     .catch(err => errorReturn(res, err));
 };
 
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then(card =>
-      /*if ((name && (name.length < 2 || name.length > 30)) || (about && (about.length < 2 || about.length > 30))) {
-        return Promise.reject(new IncorrectCardDataError('Переданы некорректные данные карточки при лайке'));
-      };*/
-      res.send({ data: card })
-    )
+    .then(card => {
+      if (card) {
+        return res.send({ data: card })
+      };
+      return Promise.reject(new CardFindError('Карточка не найден'));
+      res.send({ data: card });
+    })
     .catch(err => errorReturn(res, err));
 };
 
 const unlikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then(card => res.send({ data: card }))
+    .then(card => {
+      if (card) {
+        return res.send({ data: card })
+      };
+      return Promise.reject(new CardFindError('Карточка не найден'));
+      res.send({ data: card })
+    })
     .catch(err => errorReturn(res, err));
 };
 
