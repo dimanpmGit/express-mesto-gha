c254205cd1a5ae91b8a361c8b0a3e18cc808506c
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-27 22:35:25 +0700

    Sprint 14. Tasks 1-2 ready.

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const {
   BAD_REQUEST,
   NOT_FOUND,
@@ -24,8 +26,21 @@ const getOneUser = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.send(user))
     .catch((err) => errorReturn(res, err));
 };
--- models/user.js
+++ models/user.js
@@ -1,22 +1,42 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      default: 'Жак-Ив Кусто',
       minlength: 2,
       maxlength: 30,
     },
     about: {
       type: String,
-      required: true,
+      default: 'Исследователь',
       minlength: 2,
       maxlength: 30,
     },
     avatar: {
       type: String,
+      validate: {
+        validator: (url) => validator.isURL(url),
+        message: 'Некорректный url аватара',
+      },
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    },
+    email: {
+      type: String,
       required: true,
+      unique: true,
+      validate: {
+        validator: (email) => validator.isEmail(email),
+        message: 'Некорректный email',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
     },
   },
   { versionKey: false },
