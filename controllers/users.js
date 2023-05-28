b170e9d63f8c0e504596a1f98f2aa157d260276e
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-29 02:43:24 +0700

    Sprint 14. All ready.

--- controllers/users.js
+++ controllers/users.js
@@ -87,7 +87,7 @@ const login = (req, res, next) => {
         });
     })
     .then((user) => {
-      const token = getJwtToken({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
+      const token = getJwtToken({ _id: user._id });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
