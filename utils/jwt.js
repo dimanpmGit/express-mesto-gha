facaedb684bc27670bb6d70a4528513a34f7275d
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-05-29 04:01:56 +0700

    Sprint 14. All ready.

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,14 +1,8 @@
 /* eslint-disable eol-last */
 /* eslint-disable arrow-body-style */
 /* eslint-disable import/newline-after-import */
-const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'donottrytogetthissupersecretsecret';
 
-const getJwtToken = (id) => {
-  return jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
-};
-
 module.exports = {
-  getJwtToken,
   JWT_SECRET,
 };
